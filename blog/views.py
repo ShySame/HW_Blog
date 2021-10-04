4f383147e349bf58a4e1aab16b0b5d7d6cd2dbcb
Shy_Same <olyalychko@gmail.com>
2021-10-04 18:40:38 +0300

    paginator 2.0

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.mail import send_mail
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db.models import Count
 from django.shortcuts import redirect, render
 from django.urls import reverse
